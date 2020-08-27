d5f70472954f489987eb32fdb8ffd855039ef3b5
Muhammad Yasin Nasrulloh <70148208+yasinnasoffice@users.noreply.github.com>
2020-08-27 07:36:35 +0700

    tugas ke tiga belum selesai

---
+++ TUGAS-3/tugas3.js
@@ -0,0 +1,10 @@
+// soal 1
+console.log("saya"+" senang"+" belajar"+" javascript");
+
+// soal 2
+var katapertama="1";
+var katakedua="2";
+var kataketiga="4";
+var katakeempat="5";
+var hasil =(katapertama+1)+(katakedua+2)+(kataketiga+4)+(katakeempat)+5;
+console.log(hasil);
